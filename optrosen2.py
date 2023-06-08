d811f78f860737f45845f8f3a9e7d6d79a6c6fef
jc-sanjuan <90907121+jc-sanjuan@users.noreply.github.com>
2023-06-08 20:41:19 +0800

    Add files via upload

---
+++ optrosen2.py
@@ -0,0 +1,165 @@
+"""
+MATH 197 Z Exercise 3
+Name: Deangelo Enriquez
+Raphaell Ridao
+Jan Catherine San Juan
+Date: 13 June 2023
+"""
+
+import numpy as np 
+from numpy import *
+import math
+import sys
+
+def backtracking(fun, d, x, fx, gx, c1=1e-4, rho=0.5, alpha_in=1.0, maxback=30):
+    """
+	Parameters
+	----------
+        fun: callable
+          objective function
+		d:array
+		  current direction
+		x:array
+		  current point
+		fx:float
+		  function value at x
+		gx:array
+		  gradient at x
+        g:callable
+		  gradient of the objective function
+        ssc:string/int
+          chosen stepsize criterion
+		c1:float
+		  armijo parameter (default value is 1e-4)
+		rho:float 
+		  backtracking parameter (default value is 0.5)
+		alpha_in:float
+		  initial step length (default value is 1.0) 
+		maxback:int 
+			max number of backtracking iterations (default is 30)
+
+	Returns
+	-------
+		alpha: float
+			steplength satisfying the chosen rule or the last steplength
+	"""
+    alpha = alpha_in
+    q = np.dot(gx, d)
+    j = 0
+    armijo = fun(x + alpha*d)>fx+c1*alpha*q
+    
+    while armijo and j< maxback:
+        alpha = rho*alpha
+        j = j+1
+        armijo = fun(x + alpha*d)>fx+c1*alpha*q
+    return alpha 
+
+
+
+def fletcher_reeves(fun, x, grad, tol=1e-6, maxit=50000):
+    
+    """
+	Parameters
+	----------
+		fun:callable
+			objective function
+		x:array
+			initial point
+		gradfun:callable
+			gradient of the objective function
+		tol:float
+			tolerance of the method (default is 1e-10)
+		maxit:int
+			maximum number of iterationd
+
+	Returns
+	-------
+		tuple(x,grad_norm,it)
+			x:array
+				approximate minimizer or last iteration
+			grad_norm:float
+				norm of the gradient at x
+			it:int
+				number of iteration
+	"""
+
+
+    f = fun(x)
+    g = grad(x)
+    grad_norm = np.linalg.norm(g)
+    d = np.negative(g)
+    it = 0
+    while grad_norm>=tol and it<maxit:
+        alpha = backtracking(fun,d,x,f,-d)
+        
+        grad_normPrev = grad_norm
+        
+        x = x + alpha*d
+        g = grad(x)
+    
+        grad_norm = np.linalg.norm(grad(x))
+        B = (grad_norm**2)/(grad_normPrev**2)
+        d = np.negative(g) + np.dot(B,d)
+
+        it = it + 1
+        
+    return x,grad_norm,it
+
+
+def hager_zhang(fun, x, grad, tol=1e-6, maxit=50000):
+    
+    """
+	Parameters
+	----------
+		fun:callable
+			objective function
+		x:array
+			initial point
+		gradfun:callable
+			gradient of the objective function
+		tol:float
+			tolerance of the method (default is 1e-10)
+		maxit:int
+			maximum number of iterationd
+
+	Returns
+	-------
+		tuple(x,grad_norm,it)
+			x:array
+				approximate minimizer or last iteration
+			grad_norm:float
+				norm of the gradient at x
+			it:int
+				number of iteration
+	"""
+
+
+    f = fun(x)
+    g = grad(x)
+    grad_norm = np.linalg.norm(g)
+    d = np.negative(g)
+    it = 0
+    while grad_norm>=tol and it<maxit:
+        alpha = backtracking(fun,d,x,f,-d)
+        
+        grad_normPrev = grad_norm
+        g_old = g
+        x = x + alpha*d
+        g = grad(x)
+        grad_norm = np.linalg.norm(grad(x))
+        if it > 0:
+
+            y_old = y
+        
+            y = g - g_old
+            
+            B = np.dot((y - (2*((np.linalg.norm(y)**2)/np.dot(d,y_old))*d)),(g/np.dot(d,y_old)))
+            d = np.negative(g) + np.dot(B,d)
+        else:
+            y = g - g_old
+            #B = (grad_norm**2)/(grad_normPrev**2)
+            d = np.negative(g)
+            
+        it = it + 1
+        
+    return x,grad_norm,it
---
+++ rosenbrock.py
@@ -0,0 +1,79 @@
+"""
+MATH 197 Z Exercise 3
+Name: Deangelo Enriquez
+Raphaell Ridao
+Jan Catherine San Juan
+Date: 13 June 2023
+"""
+import numpy as np
+from optrosen2 import fletcher_reeves, hager_zhang
+import sys
+
+def rosenbrock(x):
+    """
+	Parameter
+	---------
+ 		x: array
+			input n dimensional array
+
+	Returns
+	-------
+		a:float
+            rosenbrock function
+	"""
+
+    a = 0
+    i = 0
+    while i<99:
+        a = a + 100.0*(x[i+1]-x[i]**2)**2 + (1-x[i])**2
+        i=i+1
+
+    return a
+
+def grad_rosenbrock(x):
+    """
+	Parameter
+	---------
+		x:array
+		  input n dimensional array
+
+	Returns
+	-------
+		dx:2d vector
+          gradient
+	"""
+    dx = np.zeros(100)	
+
+    i = 1
+    dx[0] = 400.0*x[0]**3 - 400.0*x[0]*x[1] + 2*x[0] - 2
+    
+    while i < 99:
+        dx[i] = dx[i] + 400.0*x[i]**3 - 400.0*x[i+1]*x[i] + 2*x[i] - 2 + 200.0*(x[i]-x[i-1]**2)
+        i = i+1
+    
+    dx[i]= 200.0*(x[i]-x[i-1]**2)
+    
+    return dx
+
+
+if __name__ == "__main__":
+    choice = input("Choose a method:\n 1. Fletcher-Reeves NLCG\n 2. Hager-Zhang NLCG\n 3. Nonlinear Barzilai–Borwein Gradient\n 4. BFGS Quasi-Newton\n 5. Dogleg Trust Region\n Input name/number: ")
+    x = np.zeros(100)
+    if choice == 'Fletcher-Reeves NLCG' or choice == '1':
+        x, grad_norm, it = fletcher_reeves(rosenbrock, x, grad_rosenbrock)
+    elif choice == 'Hager-Zhang NLCG' or choice == '2':		
+        x, grad_norm, it = hager_zhang(rosenbrock, x, grad_rosenbrock)
+    elif choice == 'Nonlinear Barzilai–Borwein Gradient' or choice == '3':		
+        print("insert function here hehe")
+    elif choice == 'BFGS Quasi-Newton' or choice == '4':		
+        print("insert function here hehe")
+    elif choice == 'Dogleg Trust Region' or choice == '5':		
+        print("insert function here hehe")
+    else:
+        print("Please input a valid number or the exact method name.")
+        sys.exit()
+        
+    print("Approximate Minimizer: {}" .format(x))
+    print("Gradient Norm 		: {}" .format(grad_norm))
+    print("Number of Iterations	: {}" .format(it))
+    print("Function Value		: {}" .format(rosenbrock(x)))
